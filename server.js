d2fed810db7decda2e3c58a0f7590ce59f5b66b9
robertodecurnex <decurnex.roberto@gmail.com>
2018-08-31 13:17:21 -0300

    Pinting / to app index.html

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/spectrodotapp/index.html'));
 app.listen(process.env.PORT || 8080);
