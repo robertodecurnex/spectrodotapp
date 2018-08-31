89c86470bb7690d7cb9b304dea772f3d9ad352ca
robertodecurnex <decurnex.roberto@gmail.com>
2018-08-31 13:01:19 -0300

    Importing OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
-  title = 'Spectro',
+  title = 'Spectro';
 
-  redirectionUrl = 'https://github.com/robertodecurnex/spectro',
+  redirectionUrl = 'https://github.com/robertodecurnex/spectro';
 
   ngOnInit() {
     window.location.replace(this.redirectionUrl);
